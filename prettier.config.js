ed048834d139c1134dc841a54a15be281a351796
Nicolas Keil <actualnickkeil@gmail.com>
2024-06-23 11:03:23 -0700

    prettier

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  singleQuote: true,
+  astroAllowShorthand: true,
+};
+
+export default config;
