0fe9dc1fd13e1a7150d3608e67899cde3cdb9356
Nicolas Keil <actualnickkeil@gmail.com>
2024-08-03 23:30:07 -0700

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nickkeil.com',
   markdown: {
     shikiConfig: {
       theme: 'rose-pine-dawn',
@@ -10,4 +12,5 @@ export default defineConfig({
     },
     rehypePlugins: [[rehypeExternalLinks, { target: '_blank' }]],
   },
+  integrations: [sitemap()],
 });
